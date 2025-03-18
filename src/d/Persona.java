ee53a9f3569e2c464fb555cf398b2809381e6a6c
Your Name <you@example.com>
2025-03-18 18:57:44 +0100

    main persona

---
+++ src/d/Persona.java
@@ -0,0 +1,50 @@
+package d;
+
+public abstract class  Persona {
+	private String nombre;
+	private int nivel;
+	private int vida;
+	private Inventario inventario;
+
+	public Persona(String nombre, int nivel, int vida, Inventario inventario) {
+		
+		this.nombre = nombre;
+		this.nivel = nivel;
+		this.vida = vida;
+		this.inventario = inventario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public Inventario getInventario() {
+		return inventario;
+	}
+
+	public void setInventario(Inventario inventario) {
+		this.inventario = inventario;
+	}
+	
+
+}
