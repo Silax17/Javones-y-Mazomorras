b0af9574d9633ad1dac2bf169b347af45f04c334
LILLK <oriolnieto2006@gmail.com>
2025-03-18 19:19:40 +0100

    added functions

--- src/d/Inventari.java
+++ src/d/Inventari.java
@@ -3,6 +3,28 @@ package d;
 import java.util.ArrayList;
 
 public class Inventari {
-	ArrayList<Equipament> equipamient = new ArrayList<Equipament>();
+	ArrayList<Equipament> equipament = new ArrayList<Equipament>();
+	
+	public Inventari(ArrayList<Equipament> equipament) {
+		this.equipament = equipament;
+	}
+
+	public ArrayList<Equipament> getEquipament() {
+		return equipament;
+	}
+
+	public void setEquipament(ArrayList<Equipament> equipament) {
+		this.equipament = equipament;
+	}
+
+	public void agregarEquipamiento(ArrayList<Equipament> equipament) {
+		this.equipament = equipament;
+	}
+	
+	public void mostrarInventario() {
+		for(int i = 0; i < equipament.size(); i++) {
+			System.out.println(equipament.get(i));
+		}
+	}
 
 }
