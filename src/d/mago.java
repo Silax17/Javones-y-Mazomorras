ca55eaad76d689e7e2ce7bbd4d691600a6455caf
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2025-03-18 19:37:53 +0100

    getters

---
+++ src/d/mago.java
@@ -0,0 +1,21 @@
+package d;
+
+public class mago extends Persona {
+	private int mana;
+
+	public mago(String nombre, int nivel, int vida, Inventario inventario, int mana) {
+		super(nombre, nivel, vida, inventario);
+		this.mana = mana;
+	}
+
+	public int getMana() {
+		return mana;
+	}
+
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+
+		
+	
+}
