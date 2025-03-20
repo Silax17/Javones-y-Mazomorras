cb68ad5de25278e5c69bbcb2e6034b1797d6bbbf
Your Name <you@example.com>
2025-03-20 16:18:38 +0100

    Update guerrero.java

--- src/d/guerrero.java
+++ src/d/guerrero.java
@@ -6,7 +6,7 @@ public class guerrero extends Persona {
 	private Persona compañeroProtegido;
 	
 	
-	public guerrero(String nombre, int nivel, int vida, Inventari inventario, boolean protegiendo, Persona compañeroProtegido) {
+	public guerrero(String nombre, int nivel, int vida, Inventario inventario, boolean protegiendo, Persona compañeroProtegido) {
 		super(nombre, nivel, vida, inventario);
 		this.protegiendo = protegiendo;
 		this.compañeroProtegido = compañeroProtegido;
@@ -32,6 +32,44 @@ public class guerrero extends Persona {
 	public void setCompañeroProtegido(Persona compañeroProtegido) {
 		this.compañeroProtegido = compañeroProtegido;
 	}
+	
+	
+	public void proteger (Persona compañero) {
+		
+		if (this.protegiendo) {
+			System.out.println("El compañero NO puede estar protegido! ");
+			
+		} else if(!this.protegiendo) {
+				this.compañeroProtegido = compañero;
+				System.out.println(getNombre() + "ha protegido a este compañero: " + compañero.getNombre());
+		}
+	}	
+	
+	public void dejardeProteger () {
+		
+		if (this.protegiendo) {
+			System.out.println(getNombre() + "le ha quitado la protección a este compañero: " + this.compañeroProtegido.getNombre());
+			
+			this.protegiendo = false; 
+			this.compañeroProtegido = null; // si el company no estar sent protegit s'assigna com a null aquell company que estava protegit //
+			
+		} else {
+			System.out.println(this.getNombre() + " no está protegiendo a nadie!  ");
+		}
+		
+	}
+	
+	@Override // persona ja conté el metode bajarVida, en aquest cas la classe guerrero conté un metode de bajarVida diferent a Persona // 
+	public void bajarVida (int vida) {
+		if (this.protegiendo) {
+			vida = vida / 2; 
+		}
+		
+	}
+	
+	
+	
+	
 	
 	
 	
