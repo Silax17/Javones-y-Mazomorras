a562376b5cedfe8f4e0733d73d9a450bf408bfe5
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2025-03-20 15:33:04 +0100

    Update mago.java

--- src/d/mago.java
+++ src/d/mago.java
@@ -1,5 +1,7 @@
 package d;
 
+import java.util.Scanner;
+
 public class mago extends Persona {
 	private int mana;
 
@@ -16,12 +18,21 @@ public class mago extends Persona {
 		this.mana = mana;
 	}
 
-	public static void lanzarHechizo(int mana) {
-		
-	}
-	
-	public static void recargarMana(int mana){
-		
-	}
+	public void lanzarHechizo(int mana) {
+		if(this.mana < mana) {
+		System.out.println("No se puede lanzar hechizo");
+		}else {
+		System.out.println("Hechizo lanzado");
+		this.mana= this.mana - mana;
+		}
+		}
+
+		public void recargarMana(int mana){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cuanto mana quieres recargar?: ");
+		mana = scan.nextInt();
+
+		this.mana = this.mana + mana;
+		}
 	
 }
