d502779023a6a5abaff83cf461d8c7b7594c2ec2
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2025-03-24 16:11:49 +0100

    Update Persona.java

--- src/d/Persona.java
+++ src/d/Persona.java
@@ -10,7 +10,7 @@ public abstract class  Persona {
 		this.nombre = nombre;
 		this.nivel = nivel;
 		this.vida = vida;
-		this.inventario = inventario;
+		this.inventario =inventario;
 	}
 
 	public String getNombre() {
